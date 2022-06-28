27ef18f4eeb8de7c6598d980a9ceb6c6fea584ea
Vegadrone <spenaantonino@gmail.com>
2022-06-28 13:16:30 +0200

    Exercise start: Create an array with objects, each object is a team member

---
+++ JS/script.js
@@ -0,0 +1,41 @@
+// Wayne Barnett	Founder & CEO	        wayne - barnett - founder - ceo.jpg
+// Angela Caroll	Chief Editor	        angela - caroll - chief - editor.jpg
+// Walter Gordon	Office Manager	        walter - gordon - office - manager.jpg
+// Angela Lopez	Social Media Manager	angela - lopez - social - media - manager.jpg
+// Scott Estrada	Developer	            scott - estrada - developer.jpg
+// Barbara Ramos	Graphic Designer	    barbara - ramos - graphic - designer.jpg
+
+
+
+const teamMembers = [
+    {
+        Name:"Wayne Barnett",
+        Role:"Founder & CEO",
+        Img: "wayne - barnett - founder - ceo.jpg"
+    },
+    {
+        Name: "Angela Caroll",
+        Role: "Chief Editor",
+        Img: " angela - caroll - chief - editor.jpg"
+    },
+    {
+        Name: "Walter Gordon",
+        Role: "Office Manager",
+        Img: "walter - gordon - office - manager.jpg"
+    },
+    {
+        Name: " Angela Lopez",
+        Role: "Media Manager",
+        Img: "angela - lopez - social - media - manager.jpg"
+    },
+    {
+        Name: "Scott Estrada",
+        Role: "Developer",
+        Img: "scott - estrada - developer.jpg"
+    },
+    {
+        Name: "Barbara Ramos",
+        Role: "Graphic Designer",
+        Img: "barbara - ramos - graphic - designer.jpg"
+    },
+];
